a020fa746ed01c1bda572add95dc7c7780788618
nehal-mantri-27 <nehalsmantri@gmail.com>
2024-04-07 21:32:34 +0530

    first commit

---
+++ lib/features/healthcare/screens/health_care_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HealthCareScreen extends StatelessWidget {
+  const HealthCareScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
